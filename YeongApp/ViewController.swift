bf775fad24b1edb248d819b4cd9b01cfa4194eb1
cg-yeong <ds1506027@gmail.com>
2022-01-10 12:21:13 +0900

    first 2

---
+++ YeongApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeongApp
+//
+//  Created by inforex on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
